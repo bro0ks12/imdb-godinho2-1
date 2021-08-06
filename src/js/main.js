e5405b032a46ad44f88a3ad11190f488b86c9e02
EST3V4M <55590586+EST3V4M@users.noreply.github.com>
2021-08-06 13:02:42 +0100

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+const swiper4col = new Swiper('.homeSwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+/*const swiper4col = new Swiper('.swiper4col', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    // Optional parameters
+    direction: 'horizontal',
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});*/
